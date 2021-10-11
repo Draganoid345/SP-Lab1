b5dae24898436566f7083b12f241e11ce7d64c34
Dragos Rusnac <dragos.rusnac@gmail.com>
2021-10-11 15:55:05 +0300

    updated Paragraph class

--- MyBook2/src/com/company/Paragraph.java
+++ MyBook2/src/com/company/Paragraph.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Paragraph {
+    private String text;
+
+    public Paragraph(String text) {
+        this.text = text;
+    }
+
+    public void print() {
+        System.out.println(this.text);
+    }
 }
