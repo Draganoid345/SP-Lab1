e51499826a3f7867829d9bbd35a60f2c5e3671d7
Dragos Rusnac <dragos.rusnac@gmail.com>
2021-10-11 15:53:45 +0300

    updated Image class

--- MyBook2/src/com/company/Image.java
+++ MyBook2/src/com/company/Image.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Image {
+    private String imageName;
+
+    public Image(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void print() {
+        System.out.println(this.imageName);
+    }
 }
