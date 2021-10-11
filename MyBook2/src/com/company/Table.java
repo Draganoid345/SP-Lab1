130d1ef946fef629f5b69d43254ee6adedc926a3
Dragos Rusnac <dragos.rusnac@gmail.com>
2021-10-11 15:55:10 +0300

    updated Table class

--- MyBook2/src/com/company/Table.java
+++ MyBook2/src/com/company/Table.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Table {
+    private String title;
+
+    public Table(String title) {
+        this.title = title;
+    }
+
+    public void print() {
+        System.out.println(this.title);
+    }
 }
