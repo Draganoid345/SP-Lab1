f2f1ebdea34c644b7b0527a81d15e8e7a241dd6b
Dragos Rusnac <dragos.rusnac@gmail.com>
2021-10-11 15:51:32 +0300

    updatet Chapter class

--- MyBook2/src/com/company/Chapter.java
+++ MyBook2/src/com/company/Chapter.java
@@ -1,4 +1,15 @@
 package com.company;
+import com.company.SubChapter;
 
 public class Chapter {
+    private String name;
+
+    public Chapter(String name) {
+        this.name = name;
+    }
+
+    public void print() {
+        System.out.println(this.name);
+
+    }
 }
